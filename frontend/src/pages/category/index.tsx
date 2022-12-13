12067359976088f98548f01c0853a0f70194c109
unknown <Savioanjos2020@gmail.com>
2022-12-13 14:23:33 -0300

    Adicionando função handleRegister

--- frontend/src/pages/category/index.tsx
+++ frontend/src/pages/category/index.tsx
@@ -1,9 +1,19 @@
+import { useState, FormEvent } from 'react';
 import Head from "next/head";
 import { Header } from "../../components/Header";
 import styles from './styles.module.scss'
 
 
  export default function Categoty() {
+
+    const [name, setName] = useState('');
+
+    async function handleRegister(event:FormEvent) {
+        event.preventDefault();
+
+        alert("CATEGORIA " + name)
+    }
+
     return (
         <>
         <Head>
@@ -15,11 +25,13 @@ import styles from './styles.module.scss'
           <main className={styles.container}>
           <h1>Cadastrar categoria</h1>
           
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleRegister}>
             <input
             type="text"
             placeholder="Digite o nome da categoria"
             className={styles.input}
+            value={name}
+            onChange={(e) => setName(e.target.value) }
             />
 
             <button className={styles.buttonAdd} type="submit" >
