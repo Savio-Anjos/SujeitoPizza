34f58985a5373649b6e0c8a26dd58c1c583574d1
unknown <Savioanjos2020@gmail.com>
2022-12-15 11:49:59 -0300

    Criando função para enviar dados para o banco de dados

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,5 +1,5 @@
 
- import { useState,  ChangeEvent } from 'react';
+ import { useState,  ChangeEvent, FormEvent } from 'react';
  import Head from "next/head";
  import styles from  './styles.module.scss';
  import { Header } from "../../components/Header";
@@ -9,6 +9,8 @@
  import { FiUpload } from "react-icons/fi";
 
  import { setupAPIClient } from '../../services/api';
+
+ import { toast } from 'react-toastify';
 
  type ItemProps = {
     id: string;
@@ -21,6 +23,11 @@
 
 
  export default function Product({ categoryList }: CategoryProps) {
+
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
+
 
 
     const [avatarUrl, setAvatarUrl] = useState('')
@@ -53,6 +60,43 @@
 
        setCategorySelected(event.target.value)
     }
+
+    async function handleRegister(event: FormEvent) {
+        event.preventDefault();
+
+        try {
+            const data = new FormData();
+
+            if(name === '' || price === '' || description === '' || imageAvatar === null) {
+                toast.error("Preencha todos os campos!");
+                return;
+            }
+
+                data.append('name', name);
+                data.append('price', price)
+                data.append('description', description);
+                data.append('category_id', categories[categorySelected].id );
+                data.append('file', imageAvatar)
+                
+                const apiClient = setupAPIClient();
+
+                await apiClient.post('/product', data);
+
+                toast.success('Cadastrado com sucesso!')
+           
+
+        } catch (err) {
+            console.log(err);
+            toast.error("Ops erro ao cadastrar!");
+        }    
+        
+        setName('');
+        setPrice('');
+        setDescription('');
+        setImageAvatar(null);
+        setAvatarUrl('');
+
+    }
     
 
     return (
@@ -67,7 +111,7 @@
             <main className={styles.container}>
                 <h1>Novo Produto</h1>
 
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleRegister}>
 
                     <label className={styles.labelAvatar}>
                         <span>
@@ -102,17 +146,23 @@
                     type="text"
                     placeholder="Digite o nome do produto" 
                     className={styles.input}
+                    value={name}
+                    onChange={(e) => {setName(e.target.value)}}
                     />
 
                    <input
                     type="text"
                     placeholder="Preço do produto" 
                     className={styles.input}
+                    value={price}
+                    onChange={(e) => {setPrice(e.target.value)}}
                     />
 
                     <textarea
                      placeholder="Descreva seu produto..."
                      className={styles.input}
+                     value={description}
+                     onChange={(e) => {setDescription(e.target.value)}}
                     />
                     
                     <button className={styles.buttonAdd} type="submit">
