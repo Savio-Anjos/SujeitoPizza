33372fe7d9d050ac476c84fe95ebd89e958dc7b9
unknown <Savioanjos2020@gmail.com>
2022-12-28 09:40:21 -0300

    Criando interface de login

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View, ActivityIndicator } from "react-native";
+import { View, ActivityIndicator, } from "react-native";
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
