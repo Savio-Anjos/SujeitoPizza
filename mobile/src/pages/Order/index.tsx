51042823ab9799b07b980e551cecb2d19c583a02
unknown <Savioanjos2020@gmail.com>
2022-12-29 11:26:46 -0300

    Recebendo parâmetros

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 
+import { useRoute, RouteProp } from '@react-navigation/native';
+
+type RouterDetailParams = {
+    Order: {
+        number: string | number;
+        order_id: string;
+    }
+}
+
+type OrderRouteProps = RouteProp<RouterDetailParams, 'Order'>;
+
 export default function Order() {
+    const route = useRoute<OrderRouteProps>();
+
     return (
         <View>
             <Text>Tela Order</Text>
+            <Text>
+                { route.params.order_id }
+            </Text>
         </View>
     )
 }
