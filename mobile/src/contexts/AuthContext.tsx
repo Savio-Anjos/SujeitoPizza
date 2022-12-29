51d79ab030589a0742796fe091e3e5eca342904f
unknown <Savioanjos2020@gmail.com>
2022-12-29 07:41:38 -0300

    Manendo o usuário logado

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, ReactNode } from 'react';
+import React, { useState, createContext, ReactNode, useEffect } from 'react';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -8,6 +8,8 @@ type AutyConTextData = {
     user: UserProps;
     isAuthenticated: boolean;
     signIn: ( credentials: SignInProps ) => Promise<void>;
+    loadingAuth: boolean;
+    loading: boolean;
 }
 
 type UserProps = {
@@ -36,9 +38,36 @@ export function AuthProvider({children}: AuthProviderProps) {
         token: '',
     })
 
-    const [loadingAuth, setLoadingAuth] = useState(false)
+    const [loadingAuth, setLoadingAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const isAuthenticated = !!user.name; 
+
+    useEffect(() => {
+       
+        async function getUser() {
+            // Pegar os dados salvos do user
+            const userInfo = await AsyncStorage.getItem('@sujeitopizzaria');
+            let hasUser: UserProps = JSON.parse(userInfo || '{}')
+
+            //Verificar se recebemos as informações dele.
+            if(Object.keys(hasUser).length > 0) {
+                api.defaults.headers.common['Authorization'] = `Bearer ${hasUser.token}`
+
+                setUser({
+                    id: hasUser.id,
+                    name: hasUser.name,
+                    email: hasUser.email,
+                    token: hasUser.token
+                })
+            }
+
+            setLoading(false);
+        }
+
+        getUser();
+
+    }, [])
 
     async function signIn({ email, password }: SignInProps) {
        setLoadingAuth(true)
@@ -79,7 +108,7 @@ export function AuthProvider({children}: AuthProviderProps) {
     }
 
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, loading, loadingAuth }}>
           {children}
         </AuthContext.Provider>
     )
