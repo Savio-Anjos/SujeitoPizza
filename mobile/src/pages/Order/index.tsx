bad91fa474a1306b4371f3b275e34ee79fbc1600
unknown <Savioanjos2020@gmail.com>
2022-12-29 16:18:26 -0300

    Adicoinando funconalidade para listar as categorias

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     View,
     Text,
@@ -20,11 +20,32 @@ type RouterDetailParams = {
     }
 }
 
+type CategoryProps = {
+    id: string;
+    name: string;
+}
+
 type OrderRouteProps = RouteProp<RouterDetailParams, 'Order'>;
 
 export default function Order() {
     const route = useRoute<OrderRouteProps>();
     const navigation = useNavigation();
+
+    const [category, setCategory] = useState<CategoryProps[] | []>([]);
+    const [categorySelected, setCategorySelected] = useState<CategoryProps>();
+
+    const [amount, setAmount] = useState('1');
+
+    useEffect(() => {
+        async function loadInfo() {
+            const response = await api.get('/category')
+            
+            setCategory(response.data);
+            setCategorySelected(response.data[0])
+        }
+
+        loadInfo();
+    }, [])
 
     async function handleCloseOrder() {
         
@@ -53,9 +74,13 @@ export default function Order() {
                 </TouchableOpacity>
             </View>
 
-            <TouchableOpacity style={styles.input}>
-                <Text style={{ color: '#FFF' }}>Pizzas</Text>
-            </TouchableOpacity>
+             {category.length !== 0 && (
+                  <TouchableOpacity style={styles.input}>
+                  <Text style={{ color: '#FFF' }}>
+                      {categorySelected?.name}
+                  </Text>
+              </TouchableOpacity>
+             )}
 
             <TouchableOpacity style={styles.input}>
                 <Text style={{ color: '#FFF' }}>Pizza de calabresa</Text>
@@ -67,7 +92,8 @@ export default function Order() {
                    style={[styles.input, { width: '60%', textAlign: 'center' }]}
                    placeholderTextColor="#F0F0F0"
                    keyboardType="numeric"
-                   value="1"
+                   value={amount}
+                   onChangeText={setAmount}
                 />
             </View>
 
