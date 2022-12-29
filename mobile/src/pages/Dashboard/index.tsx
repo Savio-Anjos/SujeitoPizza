dd68761d78d6220642edfc54801c1240d79620fb
unknown <Savioanjos2020@gmail.com>
2022-12-29 11:14:57 -0300

    Configurando funcionalidade de abrir mesa

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
-;
+
+import { useNavigation } from '@react-navigation/native';
+
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { StackParamsList } from "../../routes/app.routes";
 
 export default function Dashboard() {
+    const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>();
+
+   const [number, setNumber] = useState('');
+
+   async function openOrder() {
+     if(number === '') {
+        return;
+     }
+
+     //Precisa fazer a requisição e abrir a mesa e navegar pra proxima tela.
+     navigation.navigate('Order', {number: number, order_id: 'ea7a06e1-1888-49b2-bfe1-cc1368df07ea'})
+   }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -13,9 +29,11 @@ export default function Dashboard() {
               placeholderTextColor="#F0F0F0"
               style={styles.input}
               keyboardType="numeric"
+              value={number}
+              onChangeText={setNumber}
            />
 
-           <TouchableOpacity style={styles.button}>
+           <TouchableOpacity style={styles.button} onPress={openOrder}>
             <Text style={styles.buttonText}>Abrir mesa</Text>
            </TouchableOpacity>
 
